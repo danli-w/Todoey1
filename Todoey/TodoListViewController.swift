541e0c626aba9081143f299c66a99d217aa46365
Danli Wang <danliwang@Danlis-MacBook-Pro.local>
2019-09-08 09:20:44 -0500

    Todoey with initial

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Danli Wang on 9/8/19.
+//  Copyright © 2019 Danli Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
